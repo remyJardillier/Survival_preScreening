006b94072b4ce8faafaa03871c8adf9c73c57076
remyJardillier <remy.jardillier@laposte.net>
2021-09-30 14:47:13 +0200

    Update 1.3_pred_clin_mRNA_plot.R

--- R_scripts/1.3_pred_clin_mRNA_plot.R
+++ R_scripts/1.3_pred_clin_mRNA_plot.R
@@ -6,15 +6,15 @@ cancers_not_used <- names(p_val_test_BH[p_val_test_BH > s_level])
 print("*** Checking which clinical data is available for each cancer ***")
 
 # data frame with all the clinical features
-clin_available_mRNA <- data.frame(matrix(ncol = 6, nrow = length(cancers_all)))
+clin_available_mRNA <- data.frame(matrix(ncol = 6, nrow = length(cancers_used)))
 colnames(clin_available_mRNA) <- c("age", "gender", "grade", "T_stage", "N_stage", "M_stage")
-row.names(clin_available_mRNA) <- cancers_all
+row.names(clin_available_mRNA) <- cancers_used
 
 # vector for the plot
-clin_text_plot <- rep(NA, length(cancers_all))
-names(clin_text_plot) <- cancers_all
+clin_text_plot <- rep(NA, length(cancers_used))
+names(clin_text_plot) <- cancers_used
 
-for(cancer in cancers_all){
+for(cancer in cancers_used){
   
   print(paste("*** start learning for", cancer, "***"))
   
@@ -57,10 +57,10 @@ save(clin_text_plot, file = "clin_avalilable.RData")
 
 # p-values - Mix > clinical ? ---------------------------------------------
 
-p_val_vect_C = p_val_vect_IBS  <- rep(NA, length(cancers_all))
-names(p_val_vect_C) = names(p_val_vect_IBS)  <- cancers_all
+p_val_vect_C = p_val_vect_IBS  <- rep(NA, length(cancers_used))
+names(p_val_vect_C) = names(p_val_vect_IBS)  <- cancers_used
 
-for(cancer in cancers_all){
+for(cancer in cancers_used){
   
   load(file = paste0("data_fit/", cancer, "/ridge/pred_clin_mRNA.RData"))
   
@@ -72,80 +72,14 @@ for(cancer in cancers_all){
 
 p_val_vect_C_BH <- p.adjust(p_val_vect_C, method = "BH")
 stars_C <- sapply(p_val_vect_C, my_stars.pval)
-length(cancers_all)
+length(cancers_used)
 sum(p_val_vect_C < 0.05)
 
 p_val_vect_IBS_BH <- p.adjust(p_val_vect_IBS, method = "BH")
 stars_IBS <- sapply(p_val_vect_IBS, my_stars.pval)
-length(cancers_all)
+length(cancers_used)
 sum(p_val_vect_IBS < 0.05)
 
-# Boxplot - cancers not used -----------------------------------------------------------------
-
-# build the data frame
-C_list = IBS_list <- list()
-
-for(cancer in cancers_not_used){
-  
-  load(file = paste0("data_fit/", cancer, "/ridge/pred_clin_mRNA.RData"))
-  
-  C_list[[cancer]] <- stack(C_df_final)
-  IBS_list[[cancer]] <- stack(IBS_df_final)
-}
-
-C_final_ggplot <- melt(C_list)
-IBS_final_ggplot <- melt(IBS_list)
-
-load(file = "clin_avalilable.RData")
-
-# ggplot - C-index
-plot_clin_mRNA_C <- ggplot() + 
-  geom_boxplot(data = C_final_ggplot, aes(x = factor(L1, levels = cancers_not_used), 
-                                          y = value, fill = ind),
-               position = position_dodge(0.75)) + 
-  theme_Publication_legend_right() + xlab("Cancer") + ylab("C-index")+
-  theme(legend.title=element_blank(), legend.text = element_text(size = 15)) + ylim(NA, 1) +
-  geom_text(aes(x = 1:length(cancers_not_used), y = rep(1, length(cancers_not_used)),
-                label = stars_C[cancers_not_used]), col = "darkorchid", size = 5) +
-  scale_fill_manual(values=c("brown3","#386cb0", "orchid"),
-                    labels = c("Clinical", "mRNA", "Mixed")) +
-  geom_text(aes(x = 1:length(cancers_not_used), y = rep(0.15, length(cancers_not_used)), 
-                label = clin_text_plot[cancers_not_used]), col = "brown3", size = 4) +
-  #theme(axis.text.x = element_text(size = 14, angle = 45, hjust = 1)) +
-  geom_bracket(xmin = length(cancers_not_used) - 0.25, 
-               xmax = length(cancers_not_used) + 0.25, y.position = 0.92, 
-               label = "", col = "orchid", size = 1)
-
-plot_clin_mRNA_C
-
-
-# ggplot - IBS
-plot_clin_mRNA_IBS <- ggplot() + 
-  geom_boxplot(data = IBS_final_ggplot, aes(x = factor(L1, levels = cancers_not_used), 
-                                            y = value, fill = ind),
-               position = position_dodge(0.75)) + 
-  theme_Publication() + xlab("Cancer") + ylab("IBS")+ ylim(NA, 0.6) +
-  theme(legend.title=element_blank(), legend.text = element_text(size = 15)) + 
-  geom_text(aes(x = 1:length(cancers_not_used), y = rep(0.5, length(cancers_not_used)),
-                label = stars_IBS[cancers_not_used]), col = "darkorchid", size = 5) +
-  scale_fill_manual(values=c("brown3","#386cb0", "orchid"),
-                    labels = c("Clinical", "mRNA", "Mixed")) +
-  geom_text(aes(x = 1:length(cancers_not_used), y = rep(0, length(cancers_not_used)), 
-                label = clin_text_plot[cancers_not_used]), col = "brown3", size = 4) +
-  # theme(axis.text.x = element_text(size = 14, angle = 45, hjust = 1)) +
-  geom_bracket(xmin = length(cancers_not_used) - 0.25, 
-               xmax = length(cancers_not_used) + 0.25, y.position = 0.45, 
-               label = "", col = "orchid", size = 1)
-
-plot_clin_mRNA_IBS
-
-plot_clin_mRNA <- ggarrange(plot_clin_mRNA_C, plot_clin_mRNA_IBS, nrow = 2, ncol = 1,
-                            labels = c("A", "B"))
-plot_clin_mRNA <- annotate_figure(plot_clin_mRNA, top = text_grob("Cancers not used"))
-print(plot_clin_mRNA)
-
-ggsave(plot_clin_mRNA, filename = "pdf/suppl_clin_mRNA_can_NotUsed.pdf")
-print("Figure saved in 'pdf/suppl_clin_mRNA_can_NotUsed.pdf'")
 
 # Boxplot - cancers used -----------------------------------------------------------------
 
